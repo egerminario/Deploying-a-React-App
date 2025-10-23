af94d09688ccde1a1277c3762a87c4a24b881fef
Erika Germinario <erika.germinario@gmail.com>
2025-10-22 23:03:58 -0500

    Customization!

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="page">
+      <main className="card">
+        <h1>🌸 My App</h1>
+        <p>Hello! I’m Erika, and this is my customized demo.</p>
+        <img
+          className="smiley"
+          src="https://t4.ftcdn.net/jpg/04/31/82/57/360_F_431825797_gbFmyO0sBt29VljKwxyYxDjcV5oJCHzi.jpg"
+        />
+      </main>
+    </div>
+  );
 }
-
-export default App
